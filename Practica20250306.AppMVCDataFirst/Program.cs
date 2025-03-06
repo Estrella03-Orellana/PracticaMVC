f986ee43f541da602dfe08bc7b43a2c56b74524c
Estrella <orellanaestrella66@gmail.com>
2025-03-06 16:00:06 -0600

    Modificación de los archivos appsettings.json y el program.cs

--- Practica20250306.AppMVCDataFirst/Program.cs
+++ Practica20250306.AppMVCDataFirst/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Practica20250306.AppMVCDataFirst.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<CatalogosDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"));
+});
 
 var app = builder.Build();
 
