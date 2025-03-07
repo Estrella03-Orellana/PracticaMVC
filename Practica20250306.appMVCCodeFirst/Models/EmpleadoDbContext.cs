35074170865b7fc3366c5e79b31bb9fe4a792000
Estrella <orellanaestrella66@gmail.com>
2025-03-07 14:40:09 -0600

    Se instalaron los paquetes NuGet y se creo el contexto de la base de datos

---
+++ Practica20250306.appMVCCodeFirst/Models/EmpleadoDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Practica20250306.appMVCCodeFirst.Models
+{
+    public class EmpleadoDbContext : DbContext {
+
+        public EmpleadoDbContext(DbContextOptions<EmpleadoDbContext> options) : 
+     base(options) { }
+
+        public DbSet<Empleado> Empleado { get; set; }
+        public DbSet<Alumno> Alumno { get; set; }
+    }
+}
