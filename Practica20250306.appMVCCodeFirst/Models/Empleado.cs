b08e11ff76dd5318d7694442ce08400069cbdd4d
Estrella <orellanaestrella66@gmail.com>
2025-03-07 14:19:00 -0600

    Se creo creo la base de datos, la clase alumno y empleado para el proyecto Code First

---
+++ Practica20250306.appMVCCodeFirst/Models/Empleado.cs
@@ -0,0 +1,13 @@
+﻿namespace Practica20250306.appMVCCodeFirst.Models
+{
+    public class Empleado
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set;  }
+        public string Direccion { get; set; }
+        public string Email { get; set; }
+
+
+    }
+}
